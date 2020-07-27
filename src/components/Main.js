38bf30d3a033b74b59431dcbc9cd18cd1b92ba10
Israel Heiblum <Israel_Heiblum@amat.com>
2020-07-27 13:12:24 +0300

    added login to redux

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,50 @@
-import React, {Component} from 'react';
-import {addOne, subtractOne} from "../redux/actions/counterAction.js";
-import {connect} from "react-redux";
-import './Main.css';
+import React, { Component } from "react";
+import { addOne, subtractOne } from "../redux/actions/counterAction.js";
+import { loginUser } from "../redux/actions/loginAction.js";
+import { connect } from "react-redux";
+import "./Main.css";
 
 class Main extends Component {
-
-    render() {
-        let {count} = this.props.counter;
-        return (
-            <div className="cover">
-                <div>
-                    Counter is: {count}
-                </div>
-                <div>
-                    <button onClick={() => this.props.addOneToCounter()}>Add one</button>
-                </div>
-                <div>
-                    <button onClick={() => this.props.subtractOneFromCounter()}>Subtract one</button>
-                </div>
-            </div>
-        );
-    }
+  testMe = false;
+  render() {
+    let { count } = this.props.counter;
+    let { login } = this.props.loginUser;
+    return (
+      <React.Fragment>
+        <div className="cover login">
+          <div>
+            <button onClick={() => this.props.activeLoginUser()}>Click to Login</button>
+          </div>
+        </div>
+        <div className="cover counter">
+          <div>Counter is: {count}</div>
+          <div>
+            <button disabled={!login} onClick={() => this.props.addOneToCounter()}>
+              Add one
+            </button>
+          </div>
+          <div>
+            <button disabled={!login} onClick={() => this.props.subtractOneFromCounter()}>
+              Subtract one
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    counter: state.counter
+const mapStateToProps = (state) => ({
+  counter: state.counter,
+  loginUser: state.loginUser,
 });
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        addOneToCounter: () => dispatch(addOne()),
-        subtractOneFromCounter: () => dispatch(subtractOne())
-    }
+  return {
+    addOneToCounter: () => dispatch(addOne()),
+    subtractOneFromCounter: () => dispatch(subtractOne()),
+    activeLoginUser: () => dispatch(loginUser()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
 import counterReducer from './counterReducer';
+import loginReducer from './loginReducer';
+
 
 export default combineReducers({
-    counter: counterReducer
+    counter: counterReducer,
+    loginUser: loginReducer
 });
